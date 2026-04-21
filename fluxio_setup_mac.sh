49330806cc402b070cc5bf23768bc5d11b2e2a6d
lukavuga <luka.vuga@tscng.org>
2026-04-21 20:22:48 +0200

    Update fluxio_setup_mac.sh

--- fluxio_setup_mac.sh
+++ fluxio_setup_mac.sh
@@ -1,5 +1,12 @@
-#!/bin/zsh
-echo "[Fluxio] Configuring macOS..."
+#!/bin/bash
+echo "Setting up Fluxio for macOS..."
+
+# Omogoči Remote Login (SSH)
 sudo systemsetup -setremotelogin on
-sudo pmset -a womp 1
-echo "[Fluxio] Setup complete."
+
+# Pridobi MAC in pošlji UDP Broadcast
+MAC=$(networksetup -getmacaddress en0 | awk '{print $3}')
+echo "Sending MAC address ($MAC) to Fluxio app..."
+echo -n "FLUXIO_PAIR:$MAC" | nc -u -b 255.255.255.255 8888
+
+echo "Done!"
