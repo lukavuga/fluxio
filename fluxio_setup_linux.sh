cd39798c4c6f68d17f43ce15d86e6bd23751e5f1
lukavuga <luka.vuga@tscng.org>
2026-04-21 20:22:35 +0200

    Update fluxio_setup_linux.sh

--- fluxio_setup_linux.sh
+++ fluxio_setup_linux.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
-echo "[Fluxio] Starting Linux setup..."
-sudo apt update && sudo apt install -y openssh-server ethtool
-sudo systemctl enable --now ssh
-sudo ufw allow ssh
-INTERFACE=$(ip route get 8.8.8.8 | awk -- '{printf $5}')
-sudo ethtool -s $INTERFACE wol g
-echo "[Fluxio] Setup complete."
+echo "Setting up Fluxio for Linux..."
+
+# Namesti OpenSSH če ga ni
+sudo apt-get update && sudo apt-get install -y openssh-server net-tools
+
+# Odpri port na UFW (če obstaja)
+sudo ufw allow 22/tcp
+sudo ufw allow 8888/udp
+
+# Pridobi MAC in pošlji UDP Broadcast
+MAC=$(ip link show $(ip route | awk '/default/ { print $5 }') | awk '/ether/ { print $2 }')
+echo "Sending MAC address ($MAC) to Fluxio app..."
+echo -n "FLUXIO_PAIR:$MAC" | socat - UDP4-DATAGRAM:255.255.255.255:8888,broadcast
+
+echo "Done!"
